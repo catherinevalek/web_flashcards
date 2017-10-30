f1c4d1482fb0fd6f08e06de29070379f9d3e6de8
Catherine Valek <email": null>
2017-10-30 15:46:36 -0500

    add user edit, update, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+
+  before_action :logged_in_user, only: [:edit, :update, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+
   def index
     @users = User.all
   end
@@ -11,7 +15,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      login @user
+      log_in @user
       flash[:success] = "Welcome to Web Flashcards!"
       redirect_to @user
     else
@@ -23,9 +27,41 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Your changes have been updated!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_path
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_path unless @user == current_user
   end
 end
