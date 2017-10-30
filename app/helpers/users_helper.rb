dec66d7b516dae5e99eb1d3723256116e6406a7e
Catherine Valek <email": null>
2017-10-30 12:19:43 -0500

    uncomment Usershelper model

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,4 @@
-# module UsersHelper
+module UsersHelper
 
 #   # Returns the Gravatar for the given user.
 #   def gravatar_for(user, size: 80)
@@ -6,4 +6,4 @@
 #     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
 #     image_tag(gravatar_url, alt: user.name, class: "gravatar")
 #   end
-# end
+end
